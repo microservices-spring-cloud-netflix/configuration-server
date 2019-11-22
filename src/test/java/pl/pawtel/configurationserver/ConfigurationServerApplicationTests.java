550b18893a84572a1dc86ecdd5163edc37fd0bdf
Lipien <adam@pawtel.pl>
2019-11-22 15:26:17 +0100

    First commit.

---
+++ src/test/java/pl/pawtel/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawtel.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
